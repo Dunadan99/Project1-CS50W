9da52db5dd304e0dae018faa59937fa41010e76b
Dunadan99 <victoriochanjamenichetti@gmail.com>
2021-07-18 04:16:09 -0300

    Advances in project

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:title>", views.title, name="art"),
+    path("search/", views.search, name="search"),
+    path("random/", views.randomSite, name="random")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,44 @@
+from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-
+from django.urls import reverse
 from . import util
+import markdown2, random
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def title(request, title):
+    for filename in util.list_entries():
+        if title.lower() == filename.lower():
+            content = markdown2.markdown(util.get_entry(filename)).split('</h1>', 1)
+            content = content[0] + '</h1> \n <div id="separation"></div>' + content[1]
+            return render(request, "encyclopedia/article.html", {
+                "article": content, "title": filename + " - Encyclopedia"
+            })
+    
+    return render(request, "encyclopedia/404.html", { 
+                "name": title, "title": title + " - Encyclopedia"
+            })
+
+def search(request):
+    if request.method == "POST":
+        search = request.POST.get('q')
+        entries = []
+        for entry in util.list_entries():
+            if search.lower() == entry.lower():
+               return HttpResponseRedirect(reverse('art', args=[entry])) 
+            elif str(search.lower()) in entry.lower():
+                entries.append(entry)
+        
+        return render(request, "encyclopedia/search.html", {
+            "entries" : entries
+        })
+
+def randomSite(request):
+    entry = random.choice(util.list_entries())
+    return HttpResponseRedirect(reverse('art', args=[entry]))
+
 
